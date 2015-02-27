7a1a645484c44d2efcf524024c697336d9607824
Steven Roper <steven.m.roper@gmail.com>
2015-02-26 20:30:43 -0700

    Getting button to disable after clicked.

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,11 +1,14 @@
-var app = angular.module('directiveWorkshop', []);
+var app = angular.module('directiveWorkshop', ['myDirectives']);
 
 app.controller('mainCtrl', function($scope, mainService){
-$scope.getData = function () {
-        return mainService.getData($scope.query).then(function (data) {
-           console.log(data);
-           return $scope.data = data;
-        });
-    };
+
+	$scope.query = 'Hey';
+
+	$scope.getData = function () {
+    return mainService.getData($scope.query).then(function (data) {
+       console.log(data);
+       return $scope.data = data;
+    });
+	};
 
 });
--- myDirectives.js
+++ myDirectives.js
@@ -2,7 +2,19 @@ var app = angular.module('myDirectives', []);
 
 app.directive('pending', function() {
 	return {
+		restrict: 'A',
+		scope: {
+			request: '&',
+			searchTerm: '='
+		},
+		link: function(scope, elem, attrs) {
+			console.log(scope.searchTerm);
+			elem.bind('click', function() {
+				elem.prop('disabled', true);
+				console.log(scope.searchTerm)
+			});
 
+		}
 	}
 
 });
